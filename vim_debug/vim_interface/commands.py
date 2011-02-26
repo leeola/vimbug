70070407fecaf27b2e72393e0fb8f03b35a61570
Lee Olayvar <leeolayvar@gmail.com>
2011-02-26 15:25:53 -0800

    Added function boiler.

---
+++ vim_debug/vim_interface/commands.py
@@ -0,0 +1,16 @@
+
+
+
+def main(args):
+    ''' The main vim-debug command. Called by the user typing "Dbg [options]"
+    '''
+    pass
+
+def parse_started_args(args):
+    ''' Parse the args that Dbg will accept after it has been started.'''
+    pass
+
+def parse_stopped_args(args):
+    ''' Parse the args that Dbg will accept when it is stopped.'''
+    pass
+

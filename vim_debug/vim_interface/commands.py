b259b8440b8ace36e216ecbe5511a52bc15aa8a0
Lee Olayvar <leeolayvar@gmail.com>
2011-02-27 11:32:14 -0800

    Added exclusive groups, and the config creation/replace options.

--- vim_debug/vim_interface/commands.py
+++ vim_debug/vim_interface/commands.py
@@ -16,25 +16,35 @@ def parse_stopped_args(args):
     
     # Create a parser.
     parser = ArgumentParser(description='Start a vim-debug session.')
+    main_group = parser.add_mutually_exclusive_group()
+    config_group = parser.add_mutually_exclusive_group()
 
     # Add arguments.
-    parser.add_argument('-s', '--server', dest='server',
+    main_group.add_argument('-s', '--server', dest='server',
             default='localhost',
             help='The server vim-debug will connect to.')
-    parser.add_argument('-t', '--port', dest='port', default=9000,
+    main_group.add_argument('-t', '--port', dest='port', default=9000,
             type=int,
             help='The port used in conjunction with --server.')
-    parser.add_argument('-l', '--location', dest='location',
+    main_group.add_argument('-l', '--location', dest='location',
             help=('The location of the file/URL to debug. Note that "." is '
                 'accepted in place of the current buffer file.'))
-    parser.add_argument('-r', '--repeat', dest='repeat', action='store_true',
+    main_group.add_argument('-r', '--repeat', dest='repeat', action='store_true',
             help=('Repeat the last debug command. If other options are '
             'provided such as -s, -t, -l, etc, they will be override ' 
             'whatever is stored as the last command.'))
-    parser.add_argument('-m', '--main_debug', dest='main_debug',
+    main_group.add_argument('-m', '--main_debug', dest='main_debug',
             action='store_true',
             help=('Debug whatever file/location is in the project config. '
                 'Any additional commands supplied will overwrite whatever ' 
                 'is read in the config for that single execution, as in -r.'))
+
+    config_group.add_argument('--create_config', dest='create_config',
+            action='store_true',
+            help=('Create a config if it doesn\'t exist. Use --replace_config '
+            'if a config already exists.'))
+    config_group.add_argument('--replace_config', dest='replace_config',
+            action='store_true',
+            help='Replace an existing config file.')
 
 

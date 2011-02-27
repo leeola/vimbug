07ae596556f107235eec14d2fb2612085178b106
Lee Olayvar <leeolayvar@gmail.com>
2011-02-27 13:24:40 -0800

    Added some test boiler. Actual test coming..

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+
+import tests.collections
+
+if __name__ == '__main__':
+        tests.collections.run()
+
---
+++ tests/collections.py
@@ -0,0 +1,7 @@
+
+from attest import Tests
+from tests.interface_commands import interface_commands
+
+tests = Tests([
+    interface_commands,])
+

5a0078fc891a6dc2a18139ba7e23b64dd12369b6
Lee Olayvar <leeolayvar@gmail.com>
2011-03-01 20:50:49 -0800

    Changed the readme, and setup.py to use the readme.

--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python
 '''
-VimBug
-======
-
-VimBug is a plugin for vim that creates an integrated debugging environment
-focused on the debugging of Python applications. It is also intended to
-work with any DBGp compliant server, it just hasn't been reliably 
--- *if at all* --  tested for any servers other than Python.
-
-Note that this plugin was forked from 
-`vim-debug <http://github.com/jabapyth/vim-debug>`_ due to the author
-*(`Jared Forsyth <http://jaredforsyth.com>`_)* being on an extended trip and
-unable to respond to any pull requests. It is the intention of this project
-to one day merge with vim-debug again.
-
-Thanks for the wonderful work Jared!
 '''
 
 import setuptools
@@ -26,7 +11,7 @@ setup(
     name='vimbug',
     description=('A plugin for vim that creates an integrated debugging '
         'environment.'),
-    long_description=__doc__,
+    long_description=open('README.rst').read(),
     maintainer='Lee Olayvar',
     maintainer_email='leeolayvar@gmail.com',
     version='0.0.0',

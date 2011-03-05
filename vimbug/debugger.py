23b205e233684b97fdd41ca3e71f4ebc773b7a1b
Lee Olayvar <leeolayvar@gmail.com>
2011-03-05 14:14:26 -0800

    Added some boiler code so vimbug.main could be imported.

---
+++ vimbug/debugger.py
@@ -0,0 +1,17 @@
+'''
+'''
+import logging
+
+
+logger = logging.getLogger('vimdebug')
+
+
+class Debugger(object):
+    ''''''
+
+    def __init__(self, session_information):
+        '''
+        '''
+
+        self.session_information = session_information
+        

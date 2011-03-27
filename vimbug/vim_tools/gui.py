6287d952f6a5eb9634d29c55b5f7eafec42534ac
Lee Olayvar <leeolayvar@gmail.com>
2011-03-27 14:18:53 -0700

    More bugs squashed. The Vb command can be run without error, hazzah!

--- vimbug/vim_tools/gui.py
+++ vimbug/vim_tools/gui.py
@@ -41,6 +41,13 @@ class Buffer(object):
         # store it.
         #: The buff number as shown by `:ls`
         self._buffer_number = commands.get_buffer_number(name)
+
+    def get_number(self):
+        '''Return the number of this buffer.
+
+        Hint: It's just returning self._buffer_number
+        '''
+        return self._buffer_number
   
     def set_type(self, type):
         '''Set the type for this buffer.
@@ -54,39 +61,62 @@ class Window(object):
     '''An instance of a Vim window.'''
 
 
-    def __init__(self, id=None):
+    def __init__(self, id=None, winnr=None):
         '''
         :param id:
             If None, the current window id is used or one is generated for the
             current window if needed. If not None, a search is preformed
             looking for the supplied id, and if found an instance is created
             for that window. If one is not found, the supplied wid is created
-            for the current window.
+            for the current window. This changes when a winnr is defined.
+        :param winnr:
+            If not None, the defined winnr is selected and the id is written
+            to that winnr. If id is None and the winnr defined already has an
+            id, the instance is created with that id. Any conflicts will raise
+            the proper exceptions.
         '''
+        if winnr is None:
+            # The winnr is none. We either create an id, or search for one.
 
-        # Get the current winnr
-        current_winnr = commands.get_current_winnr()
+            if id is None:
+                # No id is specified. Try and get an id from the current winnr
+                self._id = commands.get_id_from_winnr(winnr=None)
+                return
+            else:
+                # The id is defined, so we want to see if it exists already
+                # and if not, create it for the current window (and possibly fail)
 
-        # If the given wid is None, generate one.
-        if id is None:
-            # Generate the wid
-            id = commands.find_unique_window_id()
-
-            # Assign it to the current window.
-            commands.assign_window_id(id)
+                id_winnr = commands.get_winnr_from_id(id)
+                if id_winnr is None:
+                    # There is no winnr which has that id. So assign the id to
+                    # the current winnr. This will fail if the current winnr
+                    # already has an id.
+                    commands.assign_id_to_winnr(id=id, winnr=None)
+                    # Store it and return.
+                    self._id = id
+                    return
+                else:
+                    # The id is assigned to a winnr. So simply store the id
+                    # and return
+                    self._id = id
+                    return
         else:
-            # Find the winnr for the id given, if any.
-            id_winnr = commands.get_winnr_from_id(id)
-            
-            if id_winnr is None:
-                # If there are no matching winnr ids, assign it to the current
-                # window.
-                # Note that if there is a matching id, we don't need to
-                # create/assign anything.
-                commands.assign_window_id(id)
+            # A specific winnr is being used.
 
-        #: The window id for the current window instance.
-        self._id = id
+            if id is None:
+                # No id is given though. So we find the id of that window
+                id = commands.get_id_from_winnr(winnr)
+                # Now store it and we're done.
+                self._id = id
+                return
+            else:
+                # The winnr is defined, and so is the id. So we need to try
+                # and write the id to te specified winnr.
+                commands.assign_id_to_winnr(id=id, winnr=winnr)
+                # Now if that didn't fail, store the id and return.
+                self._id = id
+                return
+        # The code path should never reach here.
 
     def command(self, command):
         '''Execute a command in the context of this window.
@@ -128,7 +158,7 @@ class Window(object):
         :param buffer:
             A :class:`Buffer` instance.
         '''
-        raise NotImplementedError()
+        commands.set_window_buffer(self._id, buffer.get_number())
     
     def split(self, plane, new_window_side, id=None):
         '''Split a window on the plane specified, either horizontal or
@@ -171,9 +201,14 @@ class Window(object):
             id=self._id,
             toggle=True)
 
-        # Now grab the current window, which should be the new window, and
-        # create it.
-        window = Window(id=id)
+        # Get the winnr of the new window. If this is a leftabove split
+        # then the winnr should be one less than this windows winnr. Otherwise
+        # it should be one more.
+        if vim_side_options[new_window_side] == 'leftabove':
+            new_winnr = commands.get_winnr_from_id(self._id) - 1
+        else:
+            new_winnr = commands.get_winnr_from_id(self._id) + 1
 
-        return window
+        # Now create the window instance and return it.
+        return Window(id=id, winnr=new_winnr)
 

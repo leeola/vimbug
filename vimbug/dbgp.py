e27200678ef7db3bda57abed4f15dfa185b629e1
Lee Olayvar <leeolayvar@gmail.com>
2011-04-13 07:20:42 -0700

    Added the dbgp module.

---
+++ vimbug/dbgp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+    vimbug.dbgp
+    ~~~~~~~~~~~
+
+    A collection of classes to handle protocol communications with a
+    DBGp connection.
+
+    :copyright: (c) 2011 by Lee Olayvar.
+    :license: MIT, see LICENSE for more details.
+'''
+
+
